a06d7cb66968a8cc2c47fb4d8e7a7d1fc2ed5879
Guillermo Escalona <geoshada@gmail.com>
2022-10-11 21:13:19 -0500

    se hace codigo para pwa

---
+++ sw.js
@@ -0,0 +1,38 @@
+const cacheName = 'sw';
+const appShellFiles = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/Js/html5-qrcode.min.js',
+  '/Js/scanner.js',
+];
+const contentToCache = appShellFiles;
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        return caches.delete(key);
+      }));
+    }));
+  });
+  self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
